187384da807c95ce79f43900294bfc57dee152df
Gary <garyivanov89@gmail.com>
2017-12-24 19:34:15 -0500

    almost completed project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 import cgi
+import re
 
 
 app = Flask(__name__)
@@ -7,22 +8,28 @@ app.config['DEBUG'] = True
 
 class Validator:
   
-  something = "hello"
-  
   def validate_input(self,text):
     space = " "
     min = 3
     max = 20  
     return (3<= len(text) <=20 and not(space in text))
   
-  def method2(self):
-    return self.method()+2
+  def password_match(self,input1,input2):
+    return (input1 == input2)
 
+  def validate_email(self,input):
+      regex = r"\w+@\w+.\w+"
+      if re.match(regex,input):
+          return (True)
+      else:
+          return (False)
+    
 
+error = ""
 
 @app.route("/")
 def index():
-    return render_template("form.html")
+    return render_template("form.html", errors=error)
 
 @app.route("/submitted", methods=['POST'])
 def submitted():
@@ -31,13 +38,35 @@ def submitted():
     password_verified = request.form['passwordv']
     email = request.form['email']
 
-    error_name = "Username is invalid" #No spaces, <3 or >20
-    error_password = "Password is invalid" #No spaces, <3 or >20
-    error_match = "Passwords do not match"
-    error_email = "Invalid Email"
 
+    errors = {
+        "error_name" : "",
+        "error_password" : "",
+        "error_match" : "",
+        "error_email" : "",
+        "contains_error" : False
+    }
 
-    return render_template("submitted.html", name = name)
+    if not Validator.validate_input('self',name):
+        errors['error_name'] = "Username is invalid" #No spaces, <3 or >20
+        errors['contains_error'] = True
+
+    if not Validator.validate_input('self',password):
+        errors['error_password'] = "Password is invalid" #No spaces, <3 or >20
+        errors['contains_error'] = True
+
+    if not Validator.password_match('self',password,password_verified):
+        errors['error_match'] = "Passwords do not match"   
+        errors['contains_error'] = True
+
+    if not Validator.validate_email('self',email):
+        errors['error_email'] = "Invalid Email"          
+        errors['contains_error'] = True
+
+    if errors['contains_error']:
+        return render_template("form.html", name=name, errors=errors)
+
+    return render_template("submitted.html", name=name)
 
 app.run()    
 
